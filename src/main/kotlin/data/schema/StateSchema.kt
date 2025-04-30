21e86c049b7c0f8237d409ddb2589270688635b0
Marwan Qashwa <marawankashwa24@gmail.com>
2025-04-30 06:32:38 +0300

    add implementation of state schema class

--- src/main/kotlin/data/schema/StateSchema.kt
+++ src/main/kotlin/data/schema/StateSchema.kt
@@ -1,22 +1,48 @@
 package com.berlin.data.schema
 
 import com.berlin.data.BaseSchema
+import com.berlin.data.StateIndex
 import com.berlin.model.State
 
 class StateSchema(
     override val fileName: String,
     override val header: List<String>
 ) : BaseSchema<State> {
-    override fun toRow(entity: State): List<String> {
-        return emptyList()
+    init {
+        require(fileName.isNotEmpty() && header.size == NUMBER_OF_ATTRIBUTES)
     }
 
-    override fun fromRow(row:List<String>): State? {
-        return null
+    override fun toRow(entity: State): List<String> {
+        return if (
+            entity.id.isEmpty() ||
+            entity.name.isEmpty() ||
+            entity.projectId.isEmpty()
+        ) emptyList()
+        else listOf(
+            entity.id,
+            entity.name,
+            entity.projectId
+        )
+    }
+
+    override fun fromRow(row: List<String>): State? {
+        return if (
+            row[StateIndex.ID].isEmpty() ||
+            row[StateIndex.NAME].isEmpty() ||
+            row[StateIndex.PROJECT_ID].isEmpty()
+        ) null
+        else State(
+            id = row[StateIndex.ID],
+            name = row[StateIndex.NAME],
+            projectId = row[StateIndex.PROJECT_ID]
+        )
     }
 
     override fun getId(entity: State): String {
-        return ""
+        return entity.id.ifEmpty { "" }
     }
 
+    private companion object {
+        const val NUMBER_OF_ATTRIBUTES = 3
+    }
 }
