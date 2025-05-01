00043723411aa9444d2104ad415cdf01000014f9
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-05-01 21:49:55 +0300

    Feature/task (#11)
    * create tasks with UI
    
    * add task repo and implementation and tests
    
    * add tests for the logic and for the ui then make the implemenation of the usecases and the ui clases
    
    * add the ui to all use cases and refactor the usecases and with typeing all test cases
    
    * fix import classes
    
    * fix conflicts again
    
    ---------
    
    Co-authored-by: dev7odaa <mah.khalil22@gmail.com>
    Co-authored-by: ahmad <ahmad@test.com>

---
+++ src/main/kotlin/presentation/task/DeleteTaskUI.kt
@@ -0,0 +1,48 @@
+package com.berlin.presentation.task
+
+import com.berlin.data.DummyData
+import com.berlin.domain.exception.InputCancelledException
+import com.berlin.domain.exception.InvalidSelectionException
+import com.berlin.domain.exception.InvalidTaskIdException
+import com.berlin.domain.usecase.task.DeleteTaskUseCase
+import com.berlin.presentation.UiRunner
+import com.berlin.presentation.helper.choose
+import com.berlin.presentation.io.Reader
+import com.berlin.presentation.io.Viewer
+
+class DeleteTaskUI(
+    private val deleteTask: DeleteTaskUseCase,
+    private val viewer: Viewer,
+    private val reader: Reader,
+) : UiRunner {
+
+    override val id: Int = 3
+    override val label: String = "Delete task"
+
+    override fun run() {
+        try {
+            val task = choose(
+                title = "Tasks",
+                elements = DummyData.tasks,
+                labelOf = { "${it.id} – ${it.title}" },
+                viewer = viewer,
+                reader = reader
+            )
+
+            viewer.show("Type Y to confirm deletion:")
+            if (!reader.read().equals("y", true)) throw InputCancelledException("")
+
+            deleteTask(task.id).onSuccess {
+                    DummyData.tasks.remove(task)
+                    viewer.show("Deleted.")
+                }.onFailure { viewer.show(it.message ?: "Deletion failed") }
+
+        } catch (ex: InputCancelledException) {
+            viewer.show("Cancelled.")
+        } catch (ex: InvalidSelectionException) {
+            viewer.show("Invalid selection")
+        } catch (ex: InvalidTaskIdException) {
+            viewer.show("invalid task id")
+        }
+    }
+}
