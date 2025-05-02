76d92dfff6c81777689b5621dd4499301b159d0b
Diyar <101934484+diyar214@users.noreply.github.com>
2025-05-02 20:14:24 +0300

    Feature/csv datasource implementation (#13)
    * add generic interface base repository,generic csv data source and add unit test for method read all,implement it
    
    * add unit test of method update
    
    * change structure of data layer
    
    * add indexes file
    
    * make entity nullable in method from row and replace todo to default value
    
    * add user schema class unit test
    
    * add implementation of user schema class
    
    * add unit test for audit,project,state,and task schemas classes
    
    * add implementation of state schema class
    
    * add implementation of project schema class,edit test to make the list contain []
    
    * add test cases for CsvDataSource
    
    * edit each class have object to id of object
    
    * add get by id
    
    * add implementation of method getAll
    
    * add implementation of task schema class ,enhance all schema classes code readability,refactor getId method,and add extra test cases to cover branches not covered
    
    * add update implementation
    
    * add write and delete implementation
    
    * refactor to more functional approach
    
    ---------
    
    Co-authored-by: Marwan Qashwa <marawankashwa24@gmail.com>

---
+++ src/main/kotlin/data/BaseSchema.kt
@@ -0,0 +1,10 @@
+package com.berlin.data
+
+interface BaseSchema<T> {
+    val fileName: String
+    val header: List<String>
+
+    fun toRow(entity: T): List<String>
+    fun fromRow(row: List<String>): T?
+    fun getId(entity: T): String?
+}
---
+++ src/main/kotlin/data/indexes.kt
@@ -0,0 +1,43 @@
+package com.berlin.data
+
+object UserIndex {
+    const val ID = 0
+    const val USER_NAME = 1
+    const val PASSWORD = 2
+    const val ROLE = 3
+}
+
+object TaskIndex {
+    const val ID = 0
+    const val PROJECT_ID = 1
+    const val TITLE = 2
+    const val DESCRIPTION = 3
+    const val STATE_ID = 4
+    const val ASSIGNED_TO = 5
+    const val CREATE_BY = 6
+    const val AUDIT_LOGS = 7
+}
+
+object StateIndex {
+    const val ID = 0
+    const val NAME = 1
+    const val PROJECT_ID = 2
+}
+
+object ProjectIndex {
+    const val ID = 0
+    const val NAME = 1
+    const val DESCRIPTION = 2
+    const val STATES_ID = 3
+    const val TASKS_ID = 4
+}
+
+object AuditLogIndex {
+    const val ID = 0
+    const val TIMES_TAMP = 1
+    const val CREATE_BY = 2
+    const val AUDIT_ACTION = 3
+    const val CHANGES_DESCRIPTION = 4
+    const val ENTITY_TYPE = 5
+    const val ENTITY_ID = 6
+}
