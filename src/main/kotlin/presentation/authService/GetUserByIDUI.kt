fbff24c85c307ff17bd767d12f0886e24a56e11e
Menna Srour <mennasrour441@gmail.com>
2025-05-03 14:08:45 +0300

    resolve conflict

--- src/main/kotlin/presentation/authService/GetUserByIDUI.kt
+++ src/main/kotlin/presentation/authService/GetUserByIDUI.kt
@@ -1,5 +1,25 @@
 package com.berlin.presentation.authService
 
-class GetUserByIDUI{
+import com.berlin.domain.usecase.authService.GetUserByIDUseCase
+import com.berlin.presentation.UiRunner
+import com.berlin.presentation.io.Reader
+import com.berlin.presentation.io.Viewer
 
+class GetUserByIDUI(
+    private val getUserByIDUseCase: GetUserByIDUseCase,
+    private val viewer: Viewer,
+    private val reader: Reader,
+) : UiRunner {
+    override val id: Int = 1
+    override val label: String = "Log in"
+    override fun run() {
+        try {
+            viewer.show("Enter the user id: ")
+            val id=(reader.read()).toString()
+            getUserByIDUseCase.getUserById(id)
+        }catch (exception:Exception){
+
+        }
+
+    }
 }
--- src/test/kotlin/presentation/authService/FetchAllUsersUITest.kt
+++ src/test/kotlin/presentation/authService/FetchAllUsersUITest.kt
@@ -1,6 +1,61 @@
 package presentation.authService
 
+import com.berlin.domain.model.User
+import com.berlin.domain.model.UserRole
+import com.berlin.domain.usecase.authService.FetchAllUsersUseCase
+import com.berlin.presentation.authService.FetchAllUsersUI
+import com.berlin.presentation.io.Viewer
+import com.google.common.truth.Truth.assertThat
+import io.mockk.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class FetchAllUsersUseCaseTest {
+    private lateinit var viewer: Viewer
+    private lateinit var useCase: FetchAllUsersUseCase
+    private lateinit var ui: FetchAllUsersUI
+    private val printed = mutableListOf<String>()
 
+    @BeforeEach
+    fun setUp() {
+        viewer = mockk(relaxed = true) {
+            every { show(capture(printed)) } just Runs
+        }
+        useCase = mockk()
+        ui = FetchAllUsersUI(useCase, viewer)
+        printed.clear()
+    }
+
+    @Test
+    fun `should print all users when users are available`() {
+        // Given
+        val users = listOf(
+            User(id = "1", userName = "Menna", password = "12345678j",  role=UserRole.ADMIN),
+            User(id = "2", userName = "Sarah", password = "1234567890",  role = UserRole.MATE)
+        )
+        every { useCase.getAllUsers() } returns users
+
+        // When
+        ui.run()
+
+        // Then
+        assertThat(printed).containsExactly(
+            "ID: 1, Name: Menna",
+            "ID: 2, Name: Sarah"
+        )
+    }
+
+    @Test
+    fun `should print message when no users are found`() {
+        // Given
+        every { useCase.getAllUsers() } returns emptyList()
+
+        // When
+        ui.run()
+
+        // Then
+        assertThat(printed).containsExactly(
+            "No users found."
+        )
+    }
 }
