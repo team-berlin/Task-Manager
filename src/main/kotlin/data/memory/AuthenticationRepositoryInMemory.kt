e41e3e0191d6ad0dc5ddf1a1180e6482625f3258
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-05-03 18:55:07 +0200

    resolve logic and create mate to hash password first check from the data

--- src/main/kotlin/data/memory/AuthenticationRepositoryInMemory.kt
+++ src/main/kotlin/data/memory/AuthenticationRepositoryInMemory.kt
@@ -2,6 +2,8 @@ package com.berlin.data.memory
 
 import com.berlin.data.DummyData.users
 import com.berlin.domain.exception.UserNotFoundException
+import com.berlin.domain.hashPassword.HashingPassword
+import com.berlin.domain.hashPassword.MD5Hasher
 import com.berlin.domain.helper.IdGenerator
 import com.berlin.domain.helper.IdGeneratorImplementation
 import com.berlin.domain.model.User
@@ -13,9 +15,11 @@ import kotlin.Result.Companion.failure
 
 class AuthRepositoryInMemory : AuthenticationRepository {
     private val userId: IdGenerator = IdGeneratorImplementation()
+    private val hashingPassword: HashingPassword =MD5Hasher()
 
     override fun login(userName: String, password: String): Result<User> {
-        val user = users.find { it.userName == userName && it.password == password }
+        val hashPassword = hashingPassword.hashPassword(password)
+        val user = users.find { it.userName == userName && it.password == hashPassword }
         return if (user != null) {
             Result.success(user)
         } else {
@@ -24,15 +28,22 @@ class AuthRepositoryInMemory : AuthenticationRepository {
     }
 
     override fun createMate(userName: String, password: String): Result<User> {
-        val newUser = User(
-            id = userId.generateId(userName),
-            userName = userName,
-            password = password,
-            permission = assignPermissions(UserRole.MATE),
-            role = UserRole.MATE
-        )
-        users.add(newUser)
-        return Result.success(newUser)
+        val checkUserName = users.find { it.userName == userName }
+        if (checkUserName != null) {
+            return failure(Exception("user name is already exist"))
+        } else {
+            val hashingPassword = hashingPassword.hashPassword(password)
+            val newUser = User(
+                id = userId.generateId(userName),
+                userName = userName,
+                password = hashingPassword,
+                permission = assignPermissions(UserRole.MATE),
+                role = UserRole.MATE
+            )
+            users.add(newUser)
+            return Result.success(newUser)
+        }
+
     }
 
     override fun getUserById(userId: String): Result<User> =
