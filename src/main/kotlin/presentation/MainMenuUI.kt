882a6fd88dc7f55f8db6f16d34923a3395e4e328
Menna Srour <mennasrour441@gmail.com>
2025-05-06 12:00:38 +0300

    refactor MainMenuUI test cases

--- src/main/kotlin/presentation/MainMenuUI.kt
+++ src/main/kotlin/presentation/MainMenuUI.kt
@@ -1,5 +1,6 @@
 package com.berlin.presentation
 
+import com.berlin.domain.model.UserRole
 import com.berlin.presentation.authService.AuthenticateUserUi
 import com.berlin.presentation.io.Reader
 import com.berlin.presentation.io.Viewer
@@ -7,17 +8,19 @@ import data.UserCache
 
 class MainMenuUI(
     private val runners: List<UiRunner>,
-    private val viewer : Viewer,
-    private val reader : Reader,
-    private val authUi: AuthenticateUserUi
+    private val viewer: Viewer,
+    private val reader: Reader,
+    private val authUi: AuthenticateUserUi,
+    private val userCache: UserCache
 ) : UiRunner {
 
     override val id = 0
     override val label = "Main menu"
 
     override fun run() {
+        viewer.show("===Welcome to our PlanMate===")
         authUi.run()
-        val currentUser = UserCache.currentUser
+        val currentUser = userCache.currentUser
         if (currentUser == null) {
             viewer.show("Login failed.")
             return
@@ -25,13 +28,10 @@ class MainMenuUI(
 
         viewer.show("===${currentUser.role}===")
 
-        val filteringRunners = when (currentUser.role.name.uppercase()) {
-            "ADMIN" -> runners.filter { it.id in adminIds }
-            "MATE" -> runners.filter { it.id in mateIds }
-            else -> {
-                viewer.show("Unknown role: ${currentUser.role}")
-                return
-            }
+        val filteringRunners = when (currentUser.role) {
+            UserRole.ADMIN -> runners.filter { it.id in adminPermissionsIds }
+            UserRole.MATE -> runners.filter { it.id in matePermissionsIds }
+
         }
 
         while (true) {
@@ -54,6 +54,6 @@ class MainMenuUI(
         viewer.show("Select an option:")
     }
 
-    private val adminIds = listOf(1, 2, 3, 4, 5, 6, 7, 30, 100, 300, 500, 900)
-    private val mateIds = listOf(1, 2, 3, 4, 5, 6, 7)
+    private val adminPermissionsIds = listOf(1, 2, 3, 4, 5, 6, 7, 30, 100, 300, 500, 900)
+    private val matePermissionsIds = listOf(1, 2, 3, 4, 5, 6, 7)
 }
