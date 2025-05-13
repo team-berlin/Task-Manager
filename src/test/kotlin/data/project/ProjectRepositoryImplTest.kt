a9a95a538e4b60aaca302bc3701674a689c5b1a5
Marwan Qashwa <marawankashwa24@gmail.com>
2025-05-13 11:02:45 +0300

    fix unit test of project repo

--- src/test/kotlin/data/project/ProjectRepositoryImplTest.kt
+++ src/test/kotlin/data/project/ProjectRepositoryImplTest.kt
@@ -5,11 +5,13 @@ import com.berlin.data.dto.ProjectDto
 import com.berlin.data.mapper.ProjectMapper
 import com.berlin.data.repository.ProjectRepositoryImpl
 import com.berlin.domain.exception.InvalidProjectException
+import com.berlin.domain.exception.ProjectNotFoundException
 import com.berlin.domain.model.Project
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 class ProjectRepositoryImplTest {
@@ -25,34 +27,26 @@ class ProjectRepositoryImplTest {
     //region createProject
 
     @Test
-    fun `createProject should return success when created succeeds`() {
+    fun `createProject should return Creation Successfully when created succeeds`() {
         // Given
         every { csvDataSource.write(any()) } returns true
+        every { projectMapper.mapToDataModel(validProject) } returns validProjectDto
         // When
         val result = repository.createProject(validProject)
         // Then
-            assertThat(result).isEqualTo("Creation Successfully")
+        assertThat(result).isEqualTo("Creation Successfully")
     }
 
     @Test
-    fun `createProject should return success with project id when created succeeds`() {
-        // Given
-        every { csvDataSource.write(any()) } returns true
-        // When
-        val result = repository.createProject(validProject)
-        // Then
-        assertThat(result).isEqualTo(validProject)
-    }
-
-
-    @Test
-    fun `createProject should return failure with InvalidProjectException when created fails`() {
+    fun `createProject should Throws InvalidProjectException when created fails`() {
         // Given
         every { csvDataSource.write(any()) } returns false
-        // When
-        val result = repository.createProject(validProject)
-        // Then
-        assertThat(result).isInstanceOf(InvalidProjectException::class.java)
+        every { projectMapper.mapToDataModel(validProject) } returns validProjectDto
+        // When // Then
+        assertThrows<InvalidProjectException> {
+            repository.createProject(validProject)
+        }
+
     }
     //endregion
 
@@ -62,6 +56,7 @@ class ProjectRepositoryImplTest {
     fun `getProjectById should return project when data source return project`() {
         // Given
         every { csvDataSource.getById(any()) } returns validProjectDto
+        every { projectMapper.mapToDomainModel(validProjectDto) } returns validProject
         // When
         val result = repository.getProjectById("projectId")
         // Then
@@ -69,45 +64,48 @@ class ProjectRepositoryImplTest {
     }
 
     @Test
-    fun `getProjectById should return null when data source return null`() {
+    fun `getProjectById should Throw ProjectNotFoundException when data source return null`() {
         // Given
         every { csvDataSource.getById(any()) } returns null
-        // When
-        val result = repository.getProjectById("projectId")
-        // Then
-        assertThat(result).isNull()
+        every { projectMapper.mapToDomainModel(validProjectDto) } returns validProject
+        // When// Then
+        assertThrows<ProjectNotFoundException> {
+            repository.getProjectById("projectId")
+        }
     }
     //endregion
 
     //region getAllProjects
 
     @Test
-    fun `getAllProjects should return null when data source return empty list`() {
+    fun `getAllProjects should return empty list when data source return empty list`() {
         // Given
         every { csvDataSource.getAll() } returns emptyList()
         // When
         val result = repository.getAllProjects()
         // Then
-        assertThat(result).isNull()
+        assertThat(result).isEqualTo(emptyList<Project>())
     }
 
     @Test
     fun `getAllProjects should return list of projects when data source return list of projects`() {
         // Given
         every { csvDataSource.getAll() } returns listOf(validProjectDto)
+        every { projectMapper.mapToDomainModel(any()) } returns validProject
         // When
         val result = repository.getAllProjects()
         // Then
-        assertThat(result).isEqualTo(projects)
+        assertThat(result).isEqualTo(listOf(validProject))
     }
     //endregion
 
     //region updateProject
 
     @Test
-    fun `updateProject should return success when updated succeeds`() {
+    fun `updateProject should return Updated Successfully when updated succeeds`() {
         // Given
         every { csvDataSource.update(any(), any()) } returns true
+        every { projectMapper.mapToDataModel(validProject) } returns validProjectDto
         // When
         val result = repository.updateProject(validProject)
         // Then
@@ -115,39 +113,21 @@ class ProjectRepositoryImplTest {
     }
 
     @Test
-    fun `updateProject should return success with project id when updated succeeds`() {
-        // Given
-        every { csvDataSource.update(any(), any()) } returns true
-        // When
-        val result = repository.updateProject(validProject)
-        // Then
-        assertThat(result).isEqualTo(validProject.id)
-    }
-
-    @Test
-    fun `updateProject should return failure when updated fails`() {
+    fun `updateProject should return throw InvalidProjectException when updated fails`() {
         // Given
         every { csvDataSource.update(any(), any()) } returns false
-        // When
-        val result = repository.updateProject(validProject)
-        // Then
-        assertThat(result).isEqualTo("can not update project")
+        every { projectMapper.mapToDataModel(validProject) } returns validProjectDto
+        // When// Then
+        assertThrows<InvalidProjectException> {
+            repository.updateProject(validProject)
+        }
     }
 
-    @Test
-    fun `updateProject should return failure with InvalidProjectException when updated fails`() {
-        // Given
-        every { csvDataSource.update(any(), any()) } returns false
-        // When
-        val result = repository.updateProject(validProject)
-        // Then
-        assertThat(result).isInstanceOf(InvalidProjectException::class.java)
-    }
     //endregion
 
     //region deleteProject
     @Test
-    fun `deleteProject should return success when deleted succeeds`() {
+    fun `deleteProject should return Deleted Successfully when deleted succeeds`() {
         // Given
         every { csvDataSource.delete(any()) } returns true
         // When
@@ -157,50 +137,22 @@ class ProjectRepositoryImplTest {
     }
 
     @Test
-    fun `deleteProject should return success with project id when deleted succeeds`() {
-        // Given
-        every { csvDataSource.delete(any()) } returns true
-        // When
-        val result = repository.deleteProject(validProject.id)
-        // Then
-        assertThat(result).isEqualTo(validProject.id)
-    }
-
-    @Test
-    fun `deleteProject should return failure when deleted fails`() {
+    fun `deleteProject should throw InvalidProjectException when deleted fails`() {
         // Given
         every { csvDataSource.delete(any()) } returns false
-        // When
-        val result = repository.deleteProject(validProject.id)
-        // Then
-        assertThat(result).isEqualTo("can not delete project")
-    }
-
-    @Test
-    fun `deleteProject should return failure with InvalidProjectException when deleted fails`() {
-        // Given
-        every { csvDataSource.delete(any()) } returns false
-        // When
-        val result = repository.deleteProject(validProject.id)
-        // Then
-        assertThat(result).isInstanceOf(InvalidProjectException::class.java)
+        // When// Then
+        assertThrows<InvalidProjectException> {
+            repository.deleteProject(validProject.id)
+        }
     }
     //endregion
 
     companion object {
-        val validProject = Project(id = "tt", title = "aaa", null,
-            null, null)
-
-        val validProjectDto = ProjectDto(id = "tt", title = "aaa", null,
-            null, null)
-
-        val projects = listOf(
-            Project(id = "tt", title = "aaa", null, null,
-                null),
-            Project(id = "dd", title = "eds", null, null,
-                null),
-            Project(id = "dds", title = "fsd", null, null,
-                null)
+        val validProject = Project(
+            id = "tt", title = "aaa", null, null, null
+        )
+        val validProjectDto = ProjectDto(
+            id = "tt", title = "aaa", null, null, null
         )
     }
 
