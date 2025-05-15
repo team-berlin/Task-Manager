7b4c394db405d0b198514dfc348539f7c119ca35
Menna Srour <111497966+MennaSrour@users.noreply.github.com>
2025-05-15 14:10:12 +0300

    use singleOf ,create repositoryModule and  refactor exceptions
    * replace single in useCaseModule and uiModule with singleOf
    
    * refactor exceptions to use base classes
    
    * create repoModule
    
    * rename repoModule to repositoryModule

--- src/main/kotlin/di/dataModule.kt
+++ src/main/kotlin/di/dataModule.kt
@@ -1,35 +1,29 @@
 package com.berlin.di
 
 import com.berlin.data.BaseDataSource
-import com.berlin.data.audit.AuditRepositoryImpl
 import com.berlin.data.csv_data_source.CsvDataSource
 import com.berlin.data.csv_data_source.schema.*
 import com.berlin.data.dto.*
 import com.berlin.data.mapper.*
 import com.berlin.data.mongodb.config.MongoConfig
 import com.berlin.data.mongodb.datasource.*
-import com.berlin.data.repository.AuthenticationRepositoryImpl
-import com.berlin.data.repository.ProjectRepositoryImpl
-import com.berlin.data.repository.TaskStateRepositoryImpl
-import com.berlin.data.repository.TaskRepositoryImpl
 import com.berlin.domain.model.*
 import com.berlin.domain.model.user.User
-import com.berlin.domain.repository.*
 import com.berlin.domain.usecase.utils.hash_algorithm.HashingString
 import com.berlin.domain.usecase.utils.hash_algorithm.MD5Hasher
 import com.berlin.domain.usecase.utils.id_generator.IdGenerator
 import com.berlin.domain.usecase.utils.id_generator.IdGeneratorImplementation
 import data.AdminUserProvider
 import data.UserCache
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
 val dataModule = module {
-    single<IdGenerator> { IdGeneratorImplementation() }
-    single<IdGeneratorImplementation> { IdGeneratorImplementation() }
-    single<HashingString> { MD5Hasher() }
+    singleOf(::IdGeneratorImplementation) bind IdGenerator::class
+    singleOf(::MD5Hasher) bind HashingString::class
 
     single { AdminUserProvider(get(named("UserDtoDataSource")), get()) }
     single { UserCache(get<AdminUserProvider>().load()) }
@@ -80,16 +74,13 @@ val dataModule = module {
             "csv_files", get(named("ProjectSchema"))
         )
     }
-
     single<BaseDataSource<UserDto>>(named("UserDtoDataSource")) {
         CsvDataSource("csv_files", get(named("UserSchema")))
     }
-
     single<BaseDataSource<Task>>(named("TaskDataSource")) { CsvDataSource("csv_files", get(named("TaskSchema"))) }
     single<BaseDataSource<TaskState>>(named("StateDataSource")) {
         CsvDataSource(
-            "csv_files",
-            get(named("StateSchema"))
+            "csv_files", get(named("StateSchema"))
         )
     }
     single<BaseDataSource<AuditLog>>(named("AuditDataSource")) { CsvDataSource("csv_files", get(named("AuditSchema"))) }
@@ -99,23 +90,5 @@ val dataModule = module {
     single { TaskStateMapper() }.bind<EntityMapper<TaskStateDto, TaskState>>()
     single { UserMapper(get()) }.bind<EntityMapper<UserDto, User>>()
     single { AuditLogMapper() }.bind<EntityMapper<AuditLogDto, AuditLog>>()
-
-    single<ProjectRepository> { ProjectRepositoryImpl(get(named("ProjectDataSource")), get<ProjectMapper>()) }
-    single<TaskRepository> { TaskRepositoryImpl(get(named("TaskDataSource")), get<TaskMapper>()) }
-    single<AuditRepository> { AuditRepositoryImpl(get(named("AuditDataSource")), get<AuditLogMapper>()) }
-    single<TaskStateRepository> {
-        TaskStateRepositoryImpl(
-            get(named("StateDataSource")),
-            get(),
-            get<TaskStateMapper>(),
-            get<TaskMapper>()
-        )
-    }
-    single<AuthenticationRepository> {
-        AuthenticationRepositoryImpl(
-            get(),
-            get(named("UserDataSource")),
-            get<UserMapper>()
-        )
-    }
 }
+
