0b2a9b5fbc91c3aa5569a071a1de503b8b22e1e9
Marwan Qashwa <100484790+marwanqashwa@users.noreply.github.com>
2025-05-16 17:47:14 +0300

    refactor methods name,replace single exp if,and use true choice before false (#70)

--- src/main/kotlin/data/csv_data_source/schema/AuditSchema.kt
+++ src/main/kotlin/data/csv_data_source/schema/AuditSchema.kt
@@ -15,20 +15,26 @@ class AuditSchema(
     }
 
     override fun toRow(entity: AuditLogDto): List<String> {
-        return if (checkAuditLogIsNotValid(entity)) emptyList()
-        else auditLogToStringsList(entity)
+        return if (checkAuditLogDtoIsNotValid(entity).not()) {
+            mapAuditLogDtoToList(entity)
+        } else {
+            emptyList()
+        }
     }
 
     override fun fromRow(row: List<String>): AuditLogDto? {
-        return if (checkRowIsNotValidAuditLog(row)) null
-        else stringsListToAuditLog(row)
+        return if (checkRowIsNotValidAuditLogDto(row).not()) {
+            mapListToAuditLogDto(row)
+        } else {
+            null
+        }
     }
 
     override fun getId(entity: AuditLogDto): String? {
         return entity.id.ifEmpty { null }
     }
 
-    private fun auditLogToStringsList(auditLog: AuditLogDto): List<String> {
+    private fun mapAuditLogDtoToList(auditLog: AuditLogDto): List<String> {
         return listOf(
             auditLog.id,
             auditLog.timestamp.toString(),
@@ -40,52 +46,60 @@ class AuditSchema(
         )
     }
 
-    private fun stringsListToAuditLog(row: List<String>): AuditLogDto {
+    private fun mapListToAuditLogDto(row: List<String>): AuditLogDto {
         return AuditLogDto(
             id = row[AuditLogIndex.ID],
-            timestamp = row[AuditLogIndex.TIMES_TAMP].toLong() ,
+            timestamp = row[AuditLogIndex.TIMES_TAMP].toLong(),
             createdByUserId = row[AuditLogIndex.CREATE_BY],
-            auditAction = stringToAuditAction(row[AuditLogIndex.AUDIT_ACTION]),
+            auditAction = mapStringToAuditAction(row[AuditLogIndex.AUDIT_ACTION]),
             changesDescription = row[AuditLogIndex.CHANGES_DESCRIPTION].ifEmpty { null },
-            entityType = stringToEntityType(row[AuditLogIndex.ENTITY_TYPE]),
+            entityType = mapStringToEntityType(row[AuditLogIndex.ENTITY_TYPE]),
             entityId = row[AuditLogIndex.ENTITY_ID]
         )
     }
 
-    private fun checkRowIsNotValidAuditLog(row: List<String>): Boolean {
-        return (row[AuditLogIndex.ID].isEmpty() ||
-                row[AuditLogIndex.TIMES_TAMP].isEmpty() ||
-                row[AuditLogIndex.CREATE_BY].isEmpty() ||
-                row[AuditLogIndex.AUDIT_ACTION].isEmpty() ||
-                row[AuditLogIndex.ENTITY_ID].isEmpty() ||
-                row[AuditLogIndex.AUDIT_ACTION] !in enumValues<AuditLog.AuditAction>().map { it.name } ||
-                row[AuditLogIndex.ENTITY_TYPE] !in enumValues<AuditLog.EntityType>().map { it.name }
-                )
+    private fun checkRowIsNotValidAuditLogDto(row: List<String>): Boolean {
+        return (row[AuditLogIndex.ID].isEmpty() || row[AuditLogIndex.TIMES_TAMP].isEmpty() || row[AuditLogIndex.CREATE_BY].isEmpty() || row[AuditLogIndex.AUDIT_ACTION].isEmpty() || row[AuditLogIndex.ENTITY_ID].isEmpty() || row[AuditLogIndex.AUDIT_ACTION] !in enumValues<AuditLog.AuditAction>().map { it.name } || row[AuditLogIndex.ENTITY_TYPE] !in enumValues<AuditLog.EntityType>().map { it.name })
     }
 
-    private fun stringToAuditAction(auditAction: String): AuditLog.AuditAction {
+    private fun mapStringToAuditAction(auditAction: String): AuditLog.AuditAction {
         return when (auditAction) {
-            AuditLog.AuditAction.CREATE.toString() -> AuditLog.AuditAction.CREATE
-            AuditLog.AuditAction.UPDATE.toString() -> AuditLog.AuditAction.UPDATE
-            AuditLog.AuditAction.DELETE.toString() -> AuditLog.AuditAction.DELETE
-            else -> AuditLog.AuditAction.CREATE
+            AuditLog.AuditAction.CREATE.toString() -> {
+                AuditLog.AuditAction.CREATE
+            }
+
+            AuditLog.AuditAction.UPDATE.toString() -> {
+                AuditLog.AuditAction.UPDATE
+            }
+
+            AuditLog.AuditAction.DELETE.toString() -> {
+                AuditLog.AuditAction.DELETE
+            }
+
+            else -> {
+                AuditLog.AuditAction.CREATE
+            }
         }
     }
 
-    private fun stringToEntityType(entityType: String): AuditLog.EntityType {
+    private fun mapStringToEntityType(entityType: String): AuditLog.EntityType {
         return when (entityType) {
-            AuditLog.EntityType.TASK.toString() -> AuditLog.EntityType.TASK
-            AuditLog.EntityType.PROJECT.toString() -> AuditLog.EntityType.PROJECT
-            else -> AuditLog.EntityType.TASK
+            AuditLog.EntityType.TASK.toString() -> {
+                AuditLog.EntityType.TASK
+            }
+
+            AuditLog.EntityType.PROJECT.toString() -> {
+                AuditLog.EntityType.PROJECT
+            }
+
+            else -> {
+                AuditLog.EntityType.TASK
+            }
         }
     }
 
-    private fun checkAuditLogIsNotValid(auditLog: AuditLogDto): Boolean {
-        return (auditLog.id.isEmpty() ||
-                auditLog.timestamp <= 0 ||
-                auditLog.createdByUserId.isEmpty() ||
-                auditLog.entityId.isEmpty()
-                )
+    private fun checkAuditLogDtoIsNotValid(auditLog: AuditLogDto): Boolean {
+        return (auditLog.id.isEmpty() || auditLog.timestamp <= 0 || auditLog.createdByUserId.isEmpty() || auditLog.entityId.isEmpty())
     }
 
     private companion object {
--- src/main/kotlin/data/csv_data_source/schema/TaskStateSchema.kt
+++ src/main/kotlin/data/csv_data_source/schema/TaskStateSchema.kt
@@ -4,8 +4,7 @@ import com.berlin.data.StateIndex
 import com.berlin.data.dto.TaskStateDto
 
 class TaskStateSchema(
-    override val fileName: String,
-    override val header: List<String>
+    override val fileName: String, override val header: List<String>
 ) : BaseSchema<TaskStateDto> {
 
     init {
@@ -13,45 +12,43 @@ class TaskStateSchema(
     }
 
     override fun toRow(entity: TaskStateDto): List<String> {
-        return if (checkStateIsNotValid(entity)) emptyList()
-        else stateToStringsList(entity)
+        return if (checkTaskStateDtoIsNotValid(entity).not()) {
+            mapTaskStateDtoToList(entity)
+        } else {
+            emptyList()
+        }
     }
 
     override fun fromRow(row: List<String>): TaskStateDto? {
-        return if (checkRowIsNotValidState(row)) null
-        else stringsListToState(row)
+        return if (checkRowIsNotValidTaskStateDto(row).not()) {
+            mapListToTaskStateDto(row)
+        } else {
+            null
+        }
     }
 
     override fun getId(entity: TaskStateDto): String? {
         return entity.id.ifEmpty { null }
     }
 
-    private fun stateToStringsList(state: TaskStateDto): List<String> {
+    private fun mapTaskStateDtoToList(state: TaskStateDto): List<String> {
         return listOf(
-            state.id,
-            state.name,
-            state.projectId
+            state.id, state.name, state.projectId
         )
     }
 
-    private fun stringsListToState(row: List<String>): TaskStateDto {
+    private fun mapListToTaskStateDto(row: List<String>): TaskStateDto {
         return TaskStateDto(
-            id = row[StateIndex.ID],
-            name = row[StateIndex.NAME],
-            projectId = row[StateIndex.PROJECT_ID]
+            id = row[StateIndex.ID], name = row[StateIndex.NAME], projectId = row[StateIndex.PROJECT_ID]
         )
     }
 
-    private fun checkRowIsNotValidState(row: List<String>): Boolean {
-        return (row[StateIndex.ID].isEmpty() ||
-                row[StateIndex.NAME].isEmpty() ||
-                row[StateIndex.PROJECT_ID].isEmpty())
+    private fun checkRowIsNotValidTaskStateDto(row: List<String>): Boolean {
+        return (row[StateIndex.ID].isEmpty() || row[StateIndex.NAME].isEmpty() || row[StateIndex.PROJECT_ID].isEmpty())
     }
 
-    private fun checkStateIsNotValid(state: TaskStateDto): Boolean {
-        return (state.id.isEmpty() ||
-                state.name.isEmpty() ||
-                state.projectId.isEmpty())
+    private fun checkTaskStateDtoIsNotValid(state: TaskStateDto): Boolean {
+        return (state.id.isEmpty() || state.name.isEmpty() || state.projectId.isEmpty())
     }
 
     private companion object {
