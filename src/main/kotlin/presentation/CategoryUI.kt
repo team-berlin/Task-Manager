5e7aa74f23683c1c0e0beade00fbb9ada66acab0
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-05-18 15:44:35 +0300

    refactor userCache currentPermission as lazy

--- src/main/kotlin/presentation/CategoryUI.kt
+++ src/main/kotlin/presentation/CategoryUI.kt
@@ -12,31 +12,36 @@ class CategoryUI(
     private val viewer: Viewer,
     private val reader: Reader,
     private val userCache: UserCache
+) : UiRunner {
 
-): UiRunner {
+    private val allowed by lazy { children.filter { it.isAllowed(userCache.currentPermission) } }
 
     override fun run() {
-        val permission = userCache.currentPermission
-        val allowed = children.filter { it.isAllowed(permission) }
-
         if (allowed.isEmpty()) {
             viewer.show("No available actions in $label.")
             return
         }
 
-        while (true) {
-            viewer.show("=== $label ===")
-            allowed.sortedBy { it.id }
-                .forEach { viewer.show("${it.id} – ${it.label}") }
-            viewer.show("X – Back")
+        executeMenu()
+    }
 
-            when (val input = reader.read()?.trim()?.uppercase(Locale.getDefault())) {
-                null,"X" -> return
-                else -> allowed
-                    .firstOrNull { it.id == input.toIntOrNull() }
-                    ?.run()
-                    ?: viewer.show("Invalid choice")
-            }
+    private fun executeMenu() {
+        while (true) {
+            showMenu()
+            val input = reader.read()?.trim()?.uppercase(Locale.getDefault())
+
+            if (input.isNullOrEmpty() || input == "X") return
+
+            allowed.firstOrNull { it.id == input.toIntOrNull() }
+                ?.run()
+                ?: viewer.show("Invalid choice")
         }
+    }
+
+    private fun showMenu() {
+        viewer.show("=== $label ===")
+        allowed.sortedBy { it.id }
+            .forEach { viewer.show("${it.id} – ${it.label}") }
+        viewer.show("X – Back")
     }
 }
